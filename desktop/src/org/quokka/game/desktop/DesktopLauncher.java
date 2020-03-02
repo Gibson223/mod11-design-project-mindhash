ea92baacd3ba129b7d72a306a8027b69c8b1e26c
xynxynxyn <t.e.pinke@student.utwente.nl>
2020-03-02 11:25:14 +0100

    Initial setup

---
+++ desktop/src/org/quokka/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package org.quokka.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import org.quokka.game.LidarGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new LidarGame(), config);
+	}
+}
