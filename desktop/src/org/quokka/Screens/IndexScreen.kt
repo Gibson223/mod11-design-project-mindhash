811f60b82772a0016bd31284e7c0f9e9dd6df23f
Gibson Vredeveld <34198702+Gibson223@users.noreply.github.com>
2020-03-22 19:36:46 +0100

    rebuild indexscreen in kotlin. file options working as well. also decluttered gui from space to get home button to work properly

--- desktop/src/org/quokka/Screens/IndexScreen.kt
+++ desktop/src/org/quokka/Screens/IndexScreen.kt
@@ -1,119 +1,92 @@
-package org.quokka.Screens;
+package org.quokka.Screens
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
-import com.badlogic.gdx.InputMultiplexer;
-import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.scenes.scene2d.InputEvent;
-import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Dialog;
-import com.badlogic.gdx.scenes.scene2d.ui.Image;
-import com.badlogic.gdx.scenes.scene2d.ui.SelectBox;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
-import com.badlogic.gdx.utils.Array;
-import com.mygdx.game.desktop.Space;
-import org.quokka.game.desktop.GameInitializer;
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.Input
+import com.badlogic.gdx.Screen
+import com.badlogic.gdx.graphics.Color
+import com.badlogic.gdx.graphics.GL20
+import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.graphics.g2d.BitmapFont
+import com.badlogic.gdx.scenes.scene2d.InputEvent
+import com.badlogic.gdx.scenes.scene2d.Stage
+import com.badlogic.gdx.scenes.scene2d.ui.Image
+import com.badlogic.gdx.scenes.scene2d.ui.SelectBox
+import com.badlogic.gdx.scenes.scene2d.ui.Skin
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener
+import org.quokka.game.desktop.GameInitializer
+import org.quokka.game.desktop.GameInitializer.settings
+import org.quokka.game.desktop.GameInitializer.updateUsedSpace
+import java.util.*
+import kotlin.system.exitProcess
 
-import java.util.ArrayList;
-import java.util.List;
+class IndexScreen : Screen {
+    var img: Image
+    var img2: Image
+    var font: BitmapFont
+    var stage: Stage
+    var skin: Skin
 
-public class IndexScreen implements Screen {
-    Texture img;
-    Texture img2;
-    BitmapFont font;
-    Stage stage;
-    Dialog dialog;
-    Skin skin;
+    init {
+        font = BitmapFont()
+        font.color = Color.BLACK
+        font.data.setScale(2f)
+        skin = Skin(Gdx.files.internal("Skins/glassy-ui.json"))
 
-   public IndexScreen(){
-       img = new Texture("UTLogo.jpg");
-       img2 = new Texture("MindhashLogo2.jpg");
-       font = new BitmapFont();
-       font.setColor(Color.BLACK);
-       font.getData().setScale(2);
-       stage = new Stage();
+        stage = Stage()
+        img = Image(Texture("UTLogo.jpg"))
+        img.setPosition(0f, Gdx.graphics.height - img.height)
+        img2 = Image(Texture("MindhashLogo2.jpg"))
+        img2.setPosition(Gdx.graphics.width - img2.width, Gdx.graphics.height - img2.height)
 
-       skin = new Skin(Gdx.files.internal("Skins/glassy-ui.json"));
-       dialog = new Dialog("", skin);
-       dialog.setSize(200,200);
-       dialog.setPosition(Gdx.graphics.getWidth()/2 - 100,Gdx.graphics.getHeight()/2 - 101);
+        stage.addActor(img)
+        stage.addActor(img2)
 
-       final SelectBox<String> selectBox = new SelectBox<String>(skin);
-       String[] files = new ArrayList<>(GameInitializer.INSTANCE.getSettings().getFiles().keySet()).toArray(new String[0]);
-       selectBox.setItems(files);
+        val badge = Image(Texture("Startbutton.png"))
+        stage.addActor(badge)
 
-       dialog.getContentTable().defaults().pad(10);
-       dialog.getContentTable().add(selectBox);
 
-       stage.addActor(dialog);
-       stage.setDebugAll(true);
+        val selectBox = SelectBox<String>(skin)
+        selectBox.width = 300f
+        selectBox.height = 100f
+        selectBox.setPosition(Gdx.graphics.width/2f - selectBox.width/2,Gdx.graphics.height /2f + selectBox.height/2)
+        stage.addActor(selectBox)
 
-       Image badge = new Image(new Texture("Startbutton.png"));
-       badge.setPosition(Gdx.graphics.getWidth()/2 - 151, Gdx.graphics.getHeight()/2 - 401);
-       badge.addListener( new ClickListener() {
-           @Override
-           public void clicked(InputEvent event, float x, float y) {
-               System.out.println("Clicked!");
-               GameInitializer.INSTANCE.updateUsedSpace(GameInitializer.INSTANCE.getSettings().getFiles().get(selectBox.getSelected()),false, false );
-           }
-       });
+        val files = ArrayList(settings.files.keys).toTypedArray()
+        selectBox.setItems(*files)
 
-       stage.addActor(badge);
-       Gdx.input.setInputProcessor(stage);
-   }
+        badge.addListener(object : ClickListener() {
+            override fun clicked(event: InputEvent, x: Float, y: Float) {
+                println("Clicked!")
+                updateUsedSpace(settings.files.getValue(selectBox.selected), local = false, axis = false)
+            }
+        })
 
-    @Override
-    public void show(){
-        Gdx.input.setInputProcessor(stage);
-        System.out.println("openend indexscreen");
-
+        badge.setPosition(Gdx.graphics.width /2 - badge.width / 2, Gdx.graphics.height*0.05f)
+        Gdx.input.inputProcessor = stage
     }
 
-    @Override
-    public void render(float delta) {
-        Gdx.gl.glClearColor(1, 1, 1, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        GameInitializer.batch.begin();
-        GameInitializer.batch.draw(img, 0, Gdx.graphics.getHeight() - 100);
-        GameInitializer.batch.draw(img2, Gdx.graphics.getWidth() - 201, Gdx.graphics.getHeight() - 100);
-        font.draw(GameInitializer.batch, "Press escape to exit the application", Gdx.graphics.getWidth() / 2  - 250, Gdx.graphics.getHeight() - 20);
-        GameInitializer.batch.end();
+    override fun show() {
+        Gdx.gl.glClearColor(1f, 1f, 1f, 1f)
+        Gdx.input.inputProcessor = stage
+        println("openend indexscreen")
+    }
 
-        stage.draw();
-        Gdx.input.setInputProcessor(stage);
+    override fun render(delta: Float) {
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT or GL20.GL_DEPTH_BUFFER_BIT)
+        GameInitializer.batch.begin()
+        font.draw(GameInitializer.batch, "Press escape to exit the application", Gdx.graphics.width / 2 - 250.toFloat(), Gdx.graphics.height - 20.toFloat())
+        GameInitializer.batch.end()
+        stage.act()
+        stage.draw()
         if (Gdx.input.isKeyPressed(Input.Keys.ESCAPE))
-        		System.exit(0);
+            exitProcess(0)
     }
 
-    @Override
-    public void resize(int width, int height) {
-
-    }
-
-    @Override
-    public void pause() {
-
-    }
-
-    @Override
-    public void resume() {
-
-    }
-
-    @Override
-    public void hide() {
-
-    }
-
-    @Override
-    public void dispose() {
-        img.dispose();
-        img2.dispose();
-        System.out.println("MyGdxGame disposed");
+    override fun resize(width: Int, height: Int) {}
+    override fun pause() {}
+    override fun resume() {}
+    override fun hide() {}
+    override fun dispose() {
+        println("MyGdxGame disposed")
     }
 }
