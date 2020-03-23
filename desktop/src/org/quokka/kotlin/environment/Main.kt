c94adc1a04f2b17eef306cdb3277814a6c414329
xynxynxyn <t.e.pinke@student.utwente.nl>
2020-03-23 02:00:40 +0100

    Modify desktop:build.gradle to allow for running and compiling from command line via 'gradlew desktop:run' and 'gradlew desktop:dist' to make a jar

--- desktop/src/org/quokka/kotlin/environment/Main.kt
+++ desktop/src/org/quokka/kotlin/environment/Main.kt
@@ -1,10 +1,10 @@
-package com.mygdx.game.desktop
+package org.quokka.kotlin.environment
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration
 import org.quokka.game.desktop.GameInitializer
 
-fun main(){
+fun main(args: Array<String>) {
     val config = LwjglApplicationConfiguration()
     config.height = 720
     config.width = 1080
